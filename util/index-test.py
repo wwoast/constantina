d7b6fb0170ce4da028fad8efb154b4fd9ea317b2
Justin Cassidy <boil.afraid@gmail.com>
2017-01-28 20:50:51 -0800

    improved index-test.py

--- util/index-test.py
+++ util/index-test.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 """
-TODO: Test this from cgi-bin and fix bugs
-also, allow test phrases to be queried
+Run this script from cgi-bin, and it will create the search indexes
+if they don't exist, and do a search for the term "Constantina".
 """
 
+from os import chdir
 from constantina import cw_search
+import ConfigParser
 
-search_results = cw_search(["sometest-phrase"])
-print search_results
+CONFIG = ConfigParser.SafeConfigParser()
+CONFIG.read('constantina.ini')
+
+ROOT_DIR = CONFIG.get("paths", "root")
+RESOURCE_DIR = CONFIG.get("paths", "resource")
+
+chdir(ROOT_DIR + "/" + RESOURCE_DIR)
+
+# one page, 20 results per page, search for Constantina, no 
+# filter-page-type terms, and no previously filtered items
+search_results = cw_search(1, 20, ["Constantina"], [], None)
+print str(search_results.hits)
