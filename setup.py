21b76eb6edf26bd2154b24a8a32f02cfb0aca3d2
Justin Cassidy <boil.afraid@gmail.com>
2017-04-02 23:33:44 -0700

    install dependencies prior to installation with "develop"

--- setup.py
+++ setup.py
@@ -135,6 +135,7 @@ class InstallPyCommand(install):
         post-install configuration.
         """
         global Settings
+        self.run_command('develop')
         Settings = read_arguments()
         self.data_files()
         install.run(self)
