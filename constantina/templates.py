96706b23fccdeac1736acae25233f82cd8f5a4cd
Justin Cassidy <boil.afraid@gmail.com>
2017-07-02 19:14:09 -0700

    hand-generate the theme menu from templates

--- constantina/templates.py
+++ constantina/templates.py
@@ -2,6 +2,57 @@ from random import randint
 from string import Template
 from shared import GlobalConfig
 
+# Anything involving template generation on server-side for Constantina
+# is here, including the preferences form menu and things to do with
+# selecting new themes.
+
+
+def template_themes(desired_theme):
+    """
+    Server side rendering of the theme selection menu.
+    """
+    theme_range = len(GlobalConfig.items('themes')) - 1
+    valid_theme = (desired_theme in xrange(0, theme_range))
+    chosen_theme = desired_theme
+    if valid_theme is False:
+        # Account for random=-1, and out-of-range values
+        chosen_theme = randint(0, theme_range)
+
+    menu = ""
+    option = Template("""
+  <label>
+     <input type="radio" name="thm" value="$theme_index" $theme_selected />
+     <img src="$theme_directory/theme.png" />
+  </label>
+""")
+
+    for theme in xrange(0, theme_range):
+        replacements = {}
+        replacements['theme_index'] = str(theme)
+        replacements['theme_directory'] = GlobalConfig.get('themes', str(theme))
+        if theme == desired_theme and valid_theme is True:
+            replacements['theme_selected'] = 'chosen="chosen"'
+        else:
+            replacements['theme_selected'] = ''
+        menu += option.safe_substitute(replacements)
+
+    random = {}
+    random['theme_index'] = -1
+    random['theme_directory'] = GlobalConfig.get('themes', str(chosen_theme))
+    random_option = Template("""
+  <label>
+     <input type="radio" name="thm" value="$theme_index" $theme_selected />
+     <img src="$theme_directory/random-theme.jpg" />
+  </label>
+""")
+    if valid_theme is False:
+        random['theme_selected'] = 'chosen="chosen"'
+    else:
+        random['theme_selected'] = ''
+    menu += random_option.safe_substitute(random)
+
+    return [menu, random['theme_directory']]
+
 
 def template_contents(raw, prefs):
     """
@@ -20,18 +71,14 @@ def template_contents(raw, prefs):
         'default_expand': 'expandAll',
         'default_revise': '120'
     }
-    theme_range = len(GlobalConfig.items('themes')) - 1
-    theme_enabled = 'checked="checked"'
-    theme_chosen = -1
     replacements = {}
 
     if prefs is None:
         # Replace page variables with defaults
         for field in missing.keys():
             replacements[field] = missing[field]
-        for theme in xrange(0, theme_range):
-            replacements['theme_state_' + str(theme)] = ''
-        replacements['theme_state_random'] = theme_enabled
+        [replacements['theme_menu'],
+         replacements['theme_directory']] = template_themes(-1)
 
     else:
         # Replace page variables with preferences
@@ -43,21 +90,8 @@ def template_contents(raw, prefs):
             'default_expand': str(prefs.gro),
             'default_revise': str(prefs.rev)
         }
-        theme_chosen = int(prefs.thm)
-        if theme_chosen not in xrange(0, theme_range):
-            replacements['theme_state_random'] = theme_enabled
-        else:
-            for theme in xrange(0, theme_range):
-                if theme_chosen == theme:
-                    replacements['theme_state_' + str(theme)] = theme_enabled
-                else:
-                    replacements['theme_state_' + str(theme)] = ''
-
-    # Based on the theme value, actually replace stylesheet theme data
-    if theme_chosen not in xrange(0, theme_range):
-        theme_chosen = randint(0, theme_range)
-    theme_directory = GlobalConfig.get("themes", str(theme_chosen))
-    replacements['theme_directory'] = theme_directory
+        [replacements['theme_menu'],
+         replacements['theme_directory']] = template_themes(int(prefs.thm))
 
     # Returned output is the template transform
     output = template.safe_substitute(replacements)
