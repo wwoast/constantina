5fa26c8ebfd81e214770d20aa4012b84d18a888e
Justin Cassidy <boil.afraid@gmail.com>
2017-07-02 18:25:12 -0700

    theme selection works, but winflat themes are super goofy due to missing styles :P

--- constantina/constantina.py
+++ constantina/constantina.py
@@ -483,6 +483,7 @@ def template_contents(raw, prefs):
     }
     theme_range = len(GlobalConfig.items('themes')) - 1
     theme_enabled = 'checked="checked"'
+    theme_chosen = -1
     replacements = {}
 
     if prefs is None:
@@ -503,14 +504,21 @@ def template_contents(raw, prefs):
             'default_expand': str(prefs.gro),
             'default_revise': str(prefs.rev)
         }
-        if int(prefs.thm) == -1:
+        theme_chosen = int(prefs.thm)
+        if theme_chosen == -1:
             replacements['theme_state_random'] = theme_enabled
         else:
             for theme in xrange(0, theme_range):
-                if int(prefs.thm) == theme:
+                if theme_chosen == theme:
                     replacements['theme_state_' + str(theme)] = theme_enabled
                 else:
                     replacements['theme_state_' + str(theme)] = ''
+
+    # Based on the theme value, actually replace stylesheet theme data
+    if theme_chosen == -1:
+        theme_chosen = randint(0, theme_range)
+    theme_directory = GlobalConfig.get("themes", str(theme_chosen))
+    replacements['theme_directory'] = theme_directory
 
     # Returned output is the template transform
     output = template.safe_substitute(replacements)
