dd05468fd85d1c7b4e236a5caab723fdf000d207
Justin Cassidy <boil.afraid@gmail.com>
2017-07-19 22:25:44 -0700

    fleshing out methods

--- constantina/zoo/cards.py
+++ constantina/zoo/cards.py
@@ -12,11 +12,73 @@ from constantina.shared import BaseFiles, BaseCardType, opendir
 syslog.openlog(ident='constantina.zoo.cards')
 
 
-class ZooCard:
+class ZooThread:
+    """
+    Linear series of ZooPost objects.
+    """
+    def __init__(self):
+        pass
+
+    def get(self, strategy):
+        """
+        Based on user preferences, get either all posts, or only the last N*10.
+        """
+        pass
+
+    def ordering(self):
+        """
+        Set the M-of-N values in the threads so we can display how many posts are
+        in a single thread.
+        """
+        pass
+
+
+class ZooPost:
     """
     Since forum cards must track updatable state in each item, we don't load
     these as HTML fragments anymore, but as raw JSON documents.
     """
+    def __init__(self):
+        pass
+
+    def __openfile(self):
+        """
+        asdf
+        """
+        pass
+
     def __interpretfile(self, thisfile):
-        """Make this support JSON as an additional type?"""
-        # TOWRITE
+        """
+        Validates that a single JSON file has all of the valid forum attributes.
+        If it doesn't, close/ignore the file, and log the failure.
+        """
+        pass
+
+    def __fresh_property(self):
+        """
+        If the post is less than a certain period old, add "fresh" attributes
+        so that we can draw this card as a "freshly updated" card.
+        """
+        pass
+
+    def first_in_thread(self):
+        """
+        If the post has no indications it is in response to another post, add
+        markup in the JSON that tells Constantina to draw this card in the base
+        page as a "start of thread" card. Likely this will be called from the 
+        Thread object.
+        """
+        pass
+
+    def __revisions(self):
+        """
+        If this thread has been revised, include the list of revisions as part of
+        the JSON for the single card, so that a user can select between them.
+        """
+        pass
+
+    def __attachments(self):
+        """
+        Validate that the links to attachments hosted by the forum are stil valid.
+        """
+        pass
